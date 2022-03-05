624522409e5a1232fa7d3117144a9e4ed7c16205
Iago W <iagowesley234@gmail.com>
2022-03-05 19:54:35 -0300

    add footer

--- footer.php
+++ footer.php
@@ -1,5 +1,97 @@
+<style>
+        body {
+            
+            margin: 0;
+            padding: 0;
+        }
+
+        p {
+            color: #fff;
+            font-size: 14px;
+        }
+        h6 {
+            color: #fff;
+            font-size: 11px;
+        }
+        h2 {
+            color: #fff;
+            font-size: 19px;
+        }
+       
+        .rodape {
+            background-color: #1F1F1F;
+            height: 100%;
+        }
+        footer {
+            width: 100%;
+            background-color: #1F1F1F;
+        }
+
+        img {
+            color: #fff;
+        }
+
+    </style>
 <footer>
-        <!-- insere o conteudo do footer aqui -->
-    </footer>
+<div class="rodape container">
+            <div class="row marcador ms-5">
+                <div class="marcador col-3">
+                    <img src="teste/logok.png" class="ms-5 mt-5 img-fluid">
+                    <p class="text-center fw-lighter mt-3">Somos uma empresa especializada em oferecer serviços voltados para viagens e programas de estudos no exterior.</p>
+                    
+                    <p class="text-center fw-lighter mt-3">CNPJ: 23.263.680/0001-31</p>
+                    <div class="row mb-5">
+                        <div class="col-6 text-center">
+                            <p class="bg-light">.....</p>
+                        </div>
+                        <div class="col-6 text-center">
+                            <p class="bg-light">.....</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="marcador col-3">
+                    <h2 class="mt-5 ms-5 mb-4">Contatos</h2>
+                    <p class=" fw-lighter ms-5">Fortaleza: +55 (85) 3182-4212 <br> São Paulo: +55 (11) 3042-4015 contato@okintercambio.com.br</p>
+                    <p class=" fw-lighter ms-5">Segunda a sexta, das 9h às 18h</p>
+                    <p class=" fw-lighter ms-5">Av. Senador Virgílio Távora, 1500, sala 11.13,<br>
+                        Edifício: The One Tower <br>
+                        Aldeota, Fortaleza/CE, Brasil <br>
+                        CEP: 60170-251</p>
+                
+                </div>  
+                <div class="marcador col-3">
+                    <h2 class="mt-5 ms-5 mb-4">OK Intercâmbio</h2>
+                    <p class="fw-lighter ms-5">Sobre</p>
+                    <p class="fw-lighter ms-5 mt-4">Destinos</p>
+                    <p class="fw-lighter ms-5 mt-4">Programas</p>
+                    <p class="fw-lighter ms-5 mt-4">Promoções</p>
+                    <p class="fw-lighter ms-5 mt-4">Blogs</p>
+                </div>
+                <div class="marcador col-3">
+                    <h2 class="mt-5 ms-5 mb-4">Redes Sociais</h2>
+                    <div class="row ms-1 mb-5">
+                        <div class="col-6 text-center">
+                            <img src="teste/whatsapp.png" alt="" width="46px" height="46px" class="">
+                        </div>
+                        <div class="col-6">
+                            <img src="teste/instagram.png" alt="" width="46px" height="46px" class="">
+                        </div>
+                    </div>
+                    <div class="row ms-1">
+                        <div class="col-6 text-center">
+                            <img src="teste/facebook.png" alt="" width="46px" height="46px" class="">
+                        </div>
+                        <div class="col-6 ">
+                            <img src="teste/youtube.png" alt="" width="50px" height="50px" class="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+        <div class="direitos-reservado mb-4">
+            <h6 class="text-uppercase text-center fw-light">Desenvolvido pela Container Digital Júnior ©2022</h6>
+            <br>
+        </div>    </footer>
 </body>
 </html>
