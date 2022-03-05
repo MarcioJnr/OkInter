4dfe4d156d9a9609a862bf0ce47a8fd93153732b
Iago W <iagowesley234@gmail.com>
2022-03-05 19:31:56 -0300

    paginas tem e contato adiconadas

---
+++ team.php
@@ -0,0 +1,232 @@
+<?php get_header(); ?>
+
+
+<main>
+    <div class="principal">
+      <div class="container-form" style=" width: 80%; height: 200px; margin: 0 auto; padding: 10px;">
+        <h2 class="fw-bold text-center"></h2>
+      </div>
+      <div class="container-form d-flex" style="margin: 0 auto; width: 80%; height: 352px;">
+
+        <div class="fale-conosco" style=" padding: 20px; width: 100%;">
+          <h4 class="fw-bold">A equipe</h4><br>
+          <p class="fw-light text-sm-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            consectetur,
+            urna sit amet tincidunt porttitor, eros Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            consectetur,
+            urna sit amet tincidunt porttitor, eros nunc iaculis sapien. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Maecenas consectetur,
+            urna Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consectetur,
+            urna sit amet tincidunt porttitor, eros nunc iaculis sapien. Nunc iaculis sapien. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Maecenas consectetur,
+            urna sit amet tinc idunt porttitor, eros nunc iaculis sapien.</p>
+          <br><br>
+          <!-- 
+          <div class="team" style="width: 100%; border: 1px solid red; margin: 0 auto; align-items: center; justify-content: center; display: flex;">
+            
+          <img src="logo-ok-intercambio-melhor-ok-intercambio-agencia-brasil-fortaleza-ceara-barata-qualidade-preco.png" class="img-fluid ms-5" alt="...">
+          <img src="logo-ok-intercambio-melhor-ok-intercambio-agencia-brasil-fortaleza-ceara-barata-qualidade-preco.png" class="img-fluid ms-5" alt="...">
+          <img src="logo-ok-intercambio-melhor-ok-intercambio-agencia-brasil-fortaleza-ceara-barata-qualidade-preco.png" class="img-fluid ms-5" alt="...">
+          <img src="logo-ok-intercambio-melhor-ok-intercambio-agencia-brasil-fortaleza-ceara-barata-qualidade-preco.png" class="img-fluid ms-5" alt="...">
+          
+         </div>
+         -->
+          <div class="container">
+            <div class="row align-items">
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b>Iago Wesley</b>
+                <p class="">Cargo</p>
+              </div>
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b>Alicer Ar</b>
+                <p class="">Cargo</p>
+              </div>
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b>Márcio Jr</b>
+                <p class="">Cargo</p>
+              </div>
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b> Guabiru</b>
+                <p class="">Cargo </p>
+              </div>
+            </div><br><br>
+            <div class="row align-items-center">
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b>Gabigol</b>
+                <p class="">Cargo </p>
+              </div>
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b>Bruno Henrique</b>
+                <p class=""> Cargo</p>
+              </div>
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b>Casimiro</b>
+                <p class="">Cargo </p>
+              </div>
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b>Marção Jr</b>
+                <p class="">Cargo</p>
+              </div>
+            </div><br><br>
+            <div class="row align-items-center">
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b> Italo</b>
+                <p class="">Cargo</p>
+              </div>
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b>Ianara Ing</b>
+                <p class="">Cargo</p>
+              </div>
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b>Guabiru</b>
+                <p class="">Cargo</p>
+              </div>
+              <div class="col text-center">
+                <img src="team1.jpg" class="img-fluid mb-5 rounded-circle" alt="">
+                <b>Serjão Berranteiro</b>
+                <p class="">Cargo</p>
+              </div>
+            </div><br><br><br><br>
+
+            <div class="trabalhe-conosco">
+              <h4 class="fw-bold">Trabalhe conosco</h4><br>
+              <p class="fw-light text-sm-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                consectetur,
+                urna sit amet tincidunt porttitor, eros Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas
+                consectetur,
+                urna sit amet tincidunt porttitor, eros nunc iaculis sapiensapiens sapien sapien.</p>
+
+
+
+              <div class="form container">
+
+                <div class="row justify-content-start">
+                  <div class="col-6 mt-4">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill" id="exampleFormControlInput1"
+                      placeholder="nome@exemplo.com">
+                  </div>
+                  <div class="col-6 mt-4">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill"
+                      id="exampleFormControlInput2"="name@example.com">
+                  </div>
+                </div>
+
+
+                <div class="row align-items-start">
+                  <div class="col-3 mt-2">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill"
+                      id="exampleFormControlInput3"="name@example.com">
+                  </div>
+                  <div class="col mt-2">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill"
+                      id="exampleFormControlInput4"="name@example.com">
+                  </div>
+                  <div class="col-3 mt2">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill"
+                      id="exampleFormControlInput5"="name@example.com">
+                  </div>
+                </div>
+
+                <div class="row align-items-start">
+                  <div class="col-2 mt-2">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill"
+                      id="exampleFormControlInput3"="name@example.com">
+                  </div>
+                  <div class="col-1 mt-2">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill"
+                      id="exampleFormControlInput4"="name@example.com">
+                  </div>
+                  <div class="col-5 mt-2">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill"
+                      id="exampleFormControlInput5"="name@example.com">
+                  </div>
+                  <div class="col-4 mt-2">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill"
+                      id="exampleFormControlInput6"="name@example.com">
+                  </div>
+                </div>
+
+                <div class="row align-items-start">
+                  <div class="col-3 mt-2">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill"
+                      id="exampleFormControlInput7"="name@example.com">
+                  </div>
+                  <div class="col-9 mt-2">
+                    <input type="email" class="form-control m-2 ms-0 rounded-pill"
+                      id="exampleFormControlInput8"="name@example.com">
+                  </div>
+
+                </div>
+                <div class="mb-3 mt-2">
+                  <textarea class="form-control rounded-3" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+
+                <div class="row justify-content">
+                  <div class="col-1" style="width: 60%;">
+                    <p class="fw-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                      consectetur,
+                      urna sit amet tincidunt porttitor, eros Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+
+                  </div>
+
+                  <div class="col-4 ms-3">
+                    <input type="submit" name="Enviar" class="btn btn-warning ms-5 col-12 rounded-pill fw-bold">
+
+                  </div>
+                    </div>
+                </div>
+<br><br><br>
+                <div class="uni" style="width: 100%; ">
+                  <h2 class="fw-bold text-center">Certificações</h2>
+                </div>
+                <br><br>
+                <div class="imgs clearfix" style="width: 100%; margin: 0 auto;">
+
+                  <img src="team1.jpg" class="rounded float-start ms-5" alt="..." style="width: 400px; height: 250px;">
+                  <img src="team1.jpg" class="rounded float-end me-5" alt="..." style="width: 400px; height: 250px;">
+            
+                </div>
+
+              </div>
+
+</div>
+
+
+              <br><br>
+
+              
+            
+
+            </div>
+
+
+            
+            <br><br>
+
+          </div>
+
+
+
+        </div>
+
+      </div>
+
+
+
+  </main>
+
+
+<?php get_footer(); ?>
