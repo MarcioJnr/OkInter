467c7f4fcc53f5e6406a874e74964ab4d681e2ed
Iago W <iagowesley234@gmail.com>
2022-03-06 00:33:47 -0300

    add logo

--- header.php
+++ header.php
@@ -52,7 +52,7 @@
     <nav class="navbar navbar-expand-lg navbar-light text-uppercase  navbar-light bg-light"
       style="width: 80%; margin: 0 auto; height: 80px;">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="assets/images/logok.png" alt="LOGO" width="100px"></a>
+        <a class="navbar-brand" href="#"><img src="<?php get_template_directory_uri(  ) .'/assets/images/logok.png'?>" alt="LOGO" width="100px"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
