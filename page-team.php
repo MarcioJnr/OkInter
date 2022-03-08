554aa0758a4f48524248281db372dce7241f11b8
Iago W <iagowesley234@gmail.com>
2022-03-08 13:24:43 -0300

    updt page-team

--- page-team.php
+++ page-team.php
@@ -2,8 +2,9 @@
 
 
 <main>
+  <br><br><br><br>
 <div class="container">
-        <div class="row">
+        <div class="row ms-4">
             <div class="col-12">
                 <h1 class="text-warning fw-bolder">Nossa equipe</h1>
                 <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisl at leo interdum consequat suscipit blandit. Non cursus turpis cursus neque placerat. Aliquam turpis elementum quisque sed. Leo commodo purus, dignissim semper. Est tortor ut platea nunc porttitor elementum suspendisse hendrerit. Eu faucibus volutpat adipiscing massa, sed diam feugiat. Leo hac tellus est, cras cursus proin condimentum pretium viverra. Turpis iaculis aliquam pellentesque ut. Interdum sodales in ultricies viverra rhoncus, purus dictumst mattis.</p>
@@ -11,94 +12,94 @@
         </div>
         <div class="row text-center mt-5">
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-5 border-warning mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 border-warning mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 border-warning mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 border-warning mb-3" >
-                <p class="fw-bold">Nome Sobrenome</p>
-                <p class="mb-4">Cargo</p>
-            </div>
-        </div>
-        <div class="row text-center mt-5">
-            <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
-                <p class="fw-bold">Nome Sobrenome</p>
-                <p class="mb-4">Cargo</p>
-            </div>
-            <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
-                <p class="fw-bold">Nome Sobrenome</p>
-                <p class="mb-4">Cargo</p>
-            </div>
-            <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
-                <p class="fw-bold">Nome Sobrenome</p>
-                <p class="mb-4">Cargo</p>
-            </div>
-            <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
         </div>
         <div class="row text-center mt-5">
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
         </div>
         <div class="row text-center mt-5">
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
             <div class="col-12 col-md-6 col-xl-3">
-                <img src="team1.jpg" alt="" class="rounded-circle border border-3 mb-3" >
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
+                <p class="fw-bold">Nome Sobrenome</p>
+                <p class="mb-4">Cargo</p>
+            </div>
+        </div>
+        <div class="row text-center mt-5">
+            <div class="col-12 col-md-6 col-xl-3">
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
+                <p class="fw-bold">Nome Sobrenome</p>
+                <p class="mb-4">Cargo</p>
+            </div>
+            <div class="col-12 col-md-6 col-xl-3">
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
+                <p class="fw-bold">Nome Sobrenome</p>
+                <p class="mb-4">Cargo</p>
+            </div>
+            <div class="col-12 col-md-6 col-xl-3">
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
+                <p class="fw-bold">Nome Sobrenome</p>
+                <p class="mb-4">Cargo</p>
+            </div>
+            <div class="col-12 col-md-6 col-xl-3">
+            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/team1.jpg" class="rounded-circle border border-5 border-warning mb-3" >
                 <p class="fw-bold">Nome Sobrenome</p>
                 <p class="mb-4">Cargo</p>
             </div>
         </div>
         <br><br>
-        <div class="row">
+        <div class="row ms-4">
             <div class="col-12">
                 <h1 class="text-warning fw-bolder">Trabalhe conosco</h1>
                 <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisl at leo interdum consequat suscipit blandit. Non cursus turpis cursus neque placerat. Aliquam turpis elementum quisque sed. Leo commodo purus, dignissim semper. Est tortor ut platea nunc porttitor elementum suspendisse hendrerit. Eu faucibus volutpat adipiscing massa, sed diam feugiat. Leo hac tellus est, cras cursus proin condimentum pretium viverra. Turpis iaculis aliquam pellentesque ut. Interdum sodales in ultricies viverra rhoncus, purus dictumst mattis.</p>
@@ -129,6 +130,15 @@
                 <div class="col-12 col-md-4 col-xl-5">
                     <label for="exampleFormControlInput1" class="form-label">Número</label>
                     <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="(88) 9 81118673">
+                    <br>
+                </div>
+                <div class="row">
+                  <div class="col-12 col-md-6 col-xl-6 ">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisl integer odio interdum netus eu arcu et duis. </p>
+                  </div>
+                  <div class="col-12 col-md-6 col-xl-6 text-center">
+                  <button type="button" class="btn btn-primary" style="width:200px; float: right;" >Enviar</button><br><br><br>
+                  </div>
                 </div>
         </div>
     </div>
