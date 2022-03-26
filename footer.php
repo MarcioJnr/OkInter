c1f6c403c5cf074878a60e5b1b657225e5e6e8b4
kingnaldoo <reinaldosn2016@gmail.com>
2022-03-25 23:39:44 -0300

    footer

--- footer.php
+++ footer.php
@@ -1,69 +1,158 @@
-    <div class="linha bg-warning" style="width: 100%; height: 10px;">
-        &nbsp;
-    </div>
-    <footer style="background-color: #190B2D;"> <br><br>
-        <div class="container">
-            <div class="row mt-0 ">
-                <div class="col-12 col-md-6 col-xl-3 text-center">
-                <img src="<?php echo get_template_directory_uri(  );?>/assets/images/logo-footer.svg">                    
-                <p class="mt-2 text-secondary">Somos uma empresa especializada em oferecer serviços voltados para viagens e programas de estudos no exterior.</p>
-                    <p class="mb-4 text-secondary">CNPJ: 23.263.680/0001-31</p>
-                    <div class="container">
-                        <div class="row mb-5">
-                            <div class="col-6">
-                                <img src="<?php echo get_template_directory_uri(  );?>/assets/images/cadastur.svg" style="width: 90%; height: 35px;">
-                            </div>
-                            <div class="col-6">
-                            <img src="<?php echo get_template_directory_uri(  );?>/assets/images/embratur.svg" style="width: 90%; height: 35px;">
-                            </div>
-                        </div>
+    <span class="bg-warning w-100" id="top-footer-detail"></span>
+
+    <footer class="d-flex justify-content-center align-items-center">
+        <div class="container py-3 mt-5">
+            <div class="d-flex flex-column flex-lg-row justify-content-between mb-5 px-2" id="content">
+                <div id="about">
+                    <div class="d-flex justify-content-center mb-4" id="logo-footer">
+                        <img class="w-50" src="<?php echo get_template_directory_uri(); ?>/assets/icons/logo.svg" alt="Ok Intercâmbio logo">
+                    </div>
+
+                    <p class="mb-4 text-center">Somos uma empresa especializada em oferecer serviços voltados para viagens e programas de estudos no exterior.</p>
+
+                    <p class="mb-4 text-center">CNPJ: 23.263.680/0001-31</p>
+
+                    <div class="d-flex justify-content-between" id="offers">
+                        <img class="rounded" src="<?php echo get_template_directory_uri(); ?>/assets/images/cadastur-logo.png" alt="Cadastur logo">
+
+                        <img class="rounded" src="<?php echo get_template_directory_uri(); ?>/assets/images/embratur-logo.png" alt="Embratur logo">
                     </div>
                 </div>
-                <div class="col-12 col-md-6 col-xl-3 ps-5">
-                    <h2 class="fw-light text-light fs-4">Contatos</h2>
-                    <p class="mt-4 text-secondary">Fortaleza +55 (88) 981989887 <br> São Paulo +55 (88) 879887765 <br> contato@okintercambio.com.br</p>
-                    <p class="text-secondary">Segunda a sexta das 9 as 12h</p>
-                    <p class="text-secondary">Av. Senador Virgílio Távora, 1500, sala 11.13,<br>
-                        Edifício: The One Tower <br>
-                        Aldeota, Fortaleza/CE, Brasil <br>
-                        CEP: 60170-251</p>
+
+                <div id="contact">
+                    <h5 class="text-warning mb-5">Contatos</h5>
+
+                    <div class="d-flex flex-column">
+                        <div class="mb-4" id="number-contact">
+                            <p class="mb-0">Fortaleza: +55 (85) 3182-4212</p>
+                            <p class="mb-0">São Paulo: +55 (11) 3042-4015</p>
+                            <p class="mb-0">contato@okintercambio.com.br</p>
+                        </div>
+
+                        <p class="mb-4">Segunda a sexta, das 9h às 18h</p>
+
+                        <p>
+                            Av. Senador Virgílio Távora, 1500, sala 11.13,
+                            Edifício: The One Tower
+                            Aldeota, Fortaleza/CE, Brasil
+                            CEP: 60170-251
+                        </p>
+                    </div>
                 </div>
-                <div class="col-12 col-md-6 col-xl-3 ps-5">
-                    <h2 class="fw-light text-light fs-4">Ok Intercâmbio</h2>
-                    <p class="mt-4 text-secondary">Sobre</p>
-                    <p class="mt-3 text-secondary">Destinos</p>
-                    <p class="mt-3 text-secondary">Programas</p>
-                    <p class="mt-3 text-secondary">Promoções</p>
-                    <p class="mt-3 text-secondary">Blog</p>
+
+                <div id="nav">
+                    <h5 class="text-warning mb-5">Ok Intercâmbio</h5>
+
+                    <div class="d-flex flex-column justify-content-between">
+                        <p><a class="text-white" href="<?php echo get_home_url(); ?>/sobre">Sobre</a></p>
+
+                        <p><a class="text-white" href="<?php echo get_home_url(); ?>/destinos">Destinos</a></p>
+
+                        <p><a class="text-white" href="<?php echo get_home_url(); ?>/programas">Programas</a></p>
+
+                        <p><a class="text-white" href="<?php echo get_home_url(); ?>/promocoes">Promoções</a></p>
+
+                        <p><a class="text-white" href="<?php echo get_home_url(); ?>/blog">Blog</a></p>
+                    </div>
                 </div>
-                <div class="col-12 col-md-6 col-xl-3 ps-5">
-                    <h2 class="fw-light text-light fs-4">Redes sociais</h2>
-                    <div class="row mt-1">
-                        <div class="col-3 col-md-6 col-xl-6">
-                            <a href="">
-                            <img src="<?php echo get_template_directory_uri(  );?>/assets/icons/facebook.svg" style="width: 46px; height: 46px;" class="mt-5">                            </a>
+
+                <div id="social-footer">
+                    <h5 class="text-warning mb-5">Redes sociais</h5>
+
+                    <div class="d-flex flex-column">
+                        <div class="row w-75 mb-3">
+                            <a href="https://pt-br.facebook.com/okintercambio/" class="col">
+                                <svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g filter="url(#filter0_d_306_5078)">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M4 10.2551C4 15.2267 7.61083 19.3609 12.3333 20.1992V12.9767H9.83333V10.1992H12.3333V7.97672C12.3333 5.47672 13.9442 4.08839 16.2225 4.08839C16.9442 4.08839 17.7225 4.19922 18.4442 4.31005V6.86589H17.1667C15.9442 6.86589 15.6667 7.47672 15.6667 8.25505V10.1992H18.3333L17.8892 12.9767H15.6667V20.1992C20.3892 19.3609 24 15.2276 24 10.2551C24 4.72422 19.5 0.199219 14 0.199219C8.5 0.199219 4 4.72422 4 10.2551Z" fill="white"/>
+                                </g>
+                                <defs>
+                                <filter id="filter0_d_306_5078" x="0" y="0.199219" width="28" height="28" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                                <feOffset dy="4"/>
+                                <feGaussianBlur stdDeviation="2"/>
+                                <feComposite in2="hardAlpha" operator="out"/>
+                                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_306_5078"/>
+                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_306_5078" result="shape"/>
+                                </filter>
+                                </defs>
+                                </svg>
+                            </a>
+
+                            <a class="col">
+                                <svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g filter="url(#filter0_d_306_5081)">
+                                <path d="M22.5043 4.8241C19.5125 0.199066 13.4044 -1.17594 8.6674 1.69908C4.05511 4.5741 2.55924 10.8241 5.55099 15.4492L5.80031 15.8242L4.80305 19.5742L8.54275 18.5742L8.91672 18.8242C10.5373 19.6992 12.2824 20.1992 14.0276 20.1992C15.8975 20.1992 17.7673 19.6992 19.3879 18.6992C24.0002 15.6992 25.3714 9.57414 22.5043 4.8241ZM19.8865 14.4492C19.3879 15.1992 18.7646 15.6992 17.892 15.8242C17.3934 15.8242 16.7701 16.0742 14.2769 15.0742C12.1578 14.0742 10.4126 12.4492 9.16603 10.5741C8.41809 9.69914 8.04412 8.57413 7.91947 7.44912C7.91947 6.44911 8.29344 5.57411 8.91672 4.9491C9.16603 4.6991 9.41534 4.5741 9.66466 4.5741H10.2879C10.5373 4.5741 10.7866 4.5741 10.9112 5.0741C11.1605 5.69911 11.7838 7.19912 11.7838 7.32412C11.9085 7.44912 11.9085 7.69912 11.7838 7.82412C11.9085 8.07413 11.7838 8.32413 11.6592 8.44913C11.5345 8.57413 11.4098 8.82413 11.2852 8.94913C11.0359 9.07413 10.9112 9.32414 11.0359 9.57414C11.5345 10.3241 12.1578 11.0741 12.7811 11.6992C13.529 12.3242 14.2769 12.8242 15.1495 13.1992C15.3989 13.3242 15.6482 13.3242 15.7728 13.0742C15.8975 12.8242 16.5208 12.1992 16.7701 11.9492C17.0194 11.6992 17.144 11.6992 17.3934 11.8242L19.3879 12.8242C19.6372 12.9492 19.8865 13.0742 20.0111 13.1992C20.1358 13.5742 20.1358 14.0742 19.8865 14.4492Z" fill="white"/>
+                                </g>
+                                <defs>
+                                <filter id="filter0_d_306_5081" x="0" y="0.199219" width="28" height="28" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                                <feOffset dy="4"/>
+                                <feGaussianBlur stdDeviation="2"/>
+                                <feComposite in2="hardAlpha" operator="out"/>
+                                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_306_5081"/>
+                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_306_5081" result="shape"/>
+                                </filter>
+                                </defs>
+                                </svg>
+                            </a>
                         </div>
-                        <div class="col-3 col-md-6 col-xl-6">
-                            <a href="">
-                            <img src="<?php echo get_template_directory_uri(  );?>/assets/icons/whatsapp.svg" style="width: 46px; height: 46px;" class="mt-5">
-                            </a>                        
-                        </div>
-                        <div class="col-3 col-md-6 col-xl-6">
-                            <a href="">
-                            <img src="<?php echo get_template_directory_uri(  );?>/assets/icons/instagram.svg" style="width: 46px; height: 46px;" class="mt-5 ms-1">
-                            </a>                        
-                        </div>
-                        <div class="col-3 col-md-6 col-xl-6">
-                            <a href="">
-                            <img src="<?php echo get_template_directory_uri(  );?>/assets/icons/youtube.svg" style="width: 46px; height: 46px;" class="mt-5">
-                            </a>                       
+                        
+                        <div class="row w-75">
+                            <a href="https://www.instagram.com/okintercambio/" class="col">
+                                <svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g filter="url(#filter0_d_873_797)">
+                                <path d="M17.2 10.1992C17.2 10.8321 17.0123 11.4508 16.6607 11.977C16.3091 12.5033 15.8093 12.9134 15.2246 13.1556C14.6399 13.3978 13.9965 13.4612 13.3757 13.3377C12.755 13.2143 12.1848 12.9095 11.7373 12.462C11.2897 12.0144 10.985 11.4442 10.8615 10.8235C10.738 10.2028 10.8014 9.55935 11.0436 8.97463C11.2858 8.38991 11.6959 7.89014 12.2222 7.53852C12.7484 7.1869 13.3671 6.99922 14 6.99922C14.8479 7.00186 15.6603 7.33985 16.2598 7.93939C16.8594 8.53894 17.1974 9.35134 17.2 10.1992ZM24 5.79922V14.5992C24 16.0844 23.41 17.5088 22.3598 18.559C21.3096 19.6092 19.8852 20.1992 18.4 20.1992H9.6C8.11479 20.1992 6.69041 19.6092 5.6402 18.559C4.59 17.5088 4 16.0844 4 14.5992V5.79922C4 4.31401 4.59 2.88962 5.6402 1.83942C6.69041 0.789217 8.11479 0.199219 9.6 0.199219H18.4C19.8852 0.199219 21.3096 0.789217 22.3598 1.83942C23.41 2.88962 24 4.31401 24 5.79922ZM18.8 10.1992C18.8 9.24987 18.5185 8.32184 17.9911 7.53248C17.4636 6.74313 16.714 6.1279 15.8369 5.7646C14.9598 5.4013 13.9947 5.30624 13.0636 5.49145C12.1325 5.67666 11.2772 6.13381 10.6059 6.80511C9.9346 7.4764 9.47744 8.33168 9.29223 9.26279C9.10702 10.1939 9.20208 11.159 9.56538 12.0361C9.92868 12.9132 10.5439 13.6628 11.3333 14.1903C12.1226 14.7177 13.0506 14.9992 14 14.9992C15.273 14.9992 16.4939 14.4935 17.3941 13.5933C18.2943 12.6932 18.8 11.4723 18.8 10.1992ZM20.4 4.99922C20.4 4.76188 20.3296 4.52987 20.1978 4.33253C20.0659 4.1352 19.8785 3.98139 19.6592 3.89056C19.4399 3.79974 19.1987 3.77597 18.9659 3.82228C18.7331 3.86858 18.5193 3.98287 18.3515 4.15069C18.1837 4.31851 18.0694 4.53233 18.0231 4.76511C17.9768 4.99789 18.0005 5.23917 18.0913 5.45844C18.1822 5.67771 18.336 5.86513 18.5333 5.99698C18.7307 6.12884 18.9627 6.19922 19.2 6.19922C19.5183 6.19922 19.8235 6.07279 20.0485 5.84775C20.2736 5.6227 20.4 5.31748 20.4 4.99922Z" fill="white"/>
+                                </g>
+                                <defs>
+                                <filter id="filter0_d_873_797" x="0" y="0.199219" width="28" height="28" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                                <feOffset dy="4"/>
+                                <feGaussianBlur stdDeviation="2"/>
+                                <feComposite in2="hardAlpha" operator="out"/>
+                                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_873_797"/>
+                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_873_797" result="shape"/>
+                                </filter>
+                                </defs>
+                                </svg>
+                            </a>
+
+                            <a href="https://www.youtube.com/OKIntercambio" class="col">
+                                <svg width="35" height="27" viewBox="0 0 35 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g filter="url(#filter0_d_306_5079)">
+                                <path d="M29.7021 2.87402C29.5527 2.3175 29.2597 1.80997 28.8525 1.40225C28.4453 0.994533 27.9382 0.700914 27.3818 0.550781C25.334 1.11759e-07 17.125 0 17.125 0C17.125 0 8.91602 -1.11759e-07 6.86816 0.547851C6.31158 0.697497 5.80417 0.990953 5.39689 1.39875C4.98961 1.80655 4.69679 2.31432 4.54785 2.87109C4 4.92188 4 9.19922 4 9.19922C4 9.19922 4 13.4766 4.54785 15.5244C4.84961 16.6553 5.74023 17.5459 6.86816 17.8477C8.91602 18.3984 17.125 18.3984 17.125 18.3984C17.125 18.3984 25.334 18.3984 27.3818 17.8477C28.5127 17.5459 29.4004 16.6553 29.7021 15.5244C30.25 13.4766 30.25 9.19922 30.25 9.19922C30.25 9.19922 30.25 4.92188 29.7021 2.87402ZM14.5176 13.125V5.27344L21.3145 9.16992L14.5176 13.125Z" fill="white"/>
+                                </g>
+                                <defs>
+                                <filter id="filter0_d_306_5079" x="0" y="0" width="34.25" height="26.3984" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                                <feOffset dy="4"/>
+                                <feGaussianBlur stdDeviation="2"/>
+                                <feComposite in2="hardAlpha" operator="out"/>
+                                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_306_5079"/>
+                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_306_5079" result="shape"/>
+                                </filter>
+                                </defs>
+                                </svg>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-                <p class="text-center text-secondary mt-5" style="text-transform: uppercase;">Desenvolvido pela Container Digital Júnior ©2022</p> <br>
+
+            <div class="mb-2" id="credits">
+                <p class="text-center mb-0">Desenvolvido pela Container Digital Júnior ©2022</p>
+            </div>
         </div>
     </footer>
+
     <?php wp_footer(); ?>
 </body>
 </html>
