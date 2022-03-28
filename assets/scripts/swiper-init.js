c91b1733d9f3c9e3cea9ba003d21dcd124738ee2
CaioBrandz <caiobrandz@gmail.com>
2022-03-28 00:58:52 -0300

    front-page + rendering depoimentos

--- assets/scripts/swiper-init.js
+++ assets/scripts/swiper-init.js
@@ -1,5 +1,5 @@
 //configs dos carrosseis
-
+//PARCEIROS
 var swiper = new Swiper('.swiper-parceiros', {
   observer: true,
   observeParents: true,
@@ -31,5 +31,36 @@ var swiper = new Swiper('.swiper-parceiros', {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+
+//DEPOIMENTOS
+var swiper = new Swiper('.swiper-depoimentos', {
+  observer: true,
+  observeParents: true,
+  setWrapperSize:true,
+  centeredSlides: true,
+  loop:true,
+  slidesPerView: 3,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction:false,
+  },
+  breakpoints: {
+    991: {
+      slidesPerView: 3,
+    },
+    767: {
+      slidesPerView: 2,
+    },
+    0: {
+      slidesPerView: 1,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
   },
 });
