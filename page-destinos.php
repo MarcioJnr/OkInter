3804ede8124f298cf0eb232aeaf0909cb54559fe
kingnaldoo <reinaldosn2016@gmail.com>
2022-04-06 18:35:28 -0300

    gambiarra/page-destinos

--- page-destinos.php
+++ page-destinos.php
@@ -43,6 +43,7 @@
                         echo '</div>';
                     endwhile;
                 endif;
+                wp_reset_postdata();
             ?>
         </div>
     </div>
@@ -77,6 +78,7 @@
                         echo '</div>';
                     endwhile;
                 endif;
+                wp_reset_postdata();
             ?>
         </div>
     </div>
@@ -84,74 +86,24 @@
     <h2 class="text-center mb-4">Depoimentos</h2>
     <h6 class="text-center mb-4">Nossos clientes falam</h6>
     <div class="container mb-5 px-0" id="depositions">
-        <div class="swiper swiper-depoimentos-destinos mt-5">
-            <div class="swiper-wrapper">
-                <?php
-                    $args = array (
-                        'post_type' => 'depoimento',
-                    );
-                    $depoimentos_query = new WP_Query($args);
-                    if($depoimentos_query->have_posts()) : while ($depoimentos_query->have_posts()) : $depoimentos_query->the_post();
-                ?>
-                <div class="swiper-slide">
-                    <div class="post-frame col-12 d-flex justify-content-center">
-                        <img class="depoimento-thumb" <?php if(!has_post_thumbnail( $post->ID )){
-                            echo "no-thumbnail";
-                            }?>" 
-                            src="<?php if(has_post_thumbnail( $post->ID )){
-                                echo get_the_post_thumbnail_url($post->ID);
-                                }
-                                else{
-                                    //alterar a imagem para um placeholder feito
-                                    echo get_template_directory_uri()."/assets/images/ret larg 2.jpg";
-                                }?>
-                            " 
-                            alt="<?php the_title();?>"
-                        >
-                    </div>
-                    <div class="depo-info col-11"> 
-                        <div class="depo-category">
-                            <p style="margin-bottom:0"><?php the_category(', ');?></p>
-                            
-                        </div>
-                        <div class="depo-title col-12 text-center">
-                            <h6 style="line-height: inherit;" class="fw-bold">
-                                <?php 
-                                    if (strlen($post->post_title) > 25) {
-                                        echo substr(the_title($before = '', $after = '', FALSE), 0, 25) . '...'; } 
-                                        else {
-                                        the_title();
-                                    } 
-                                ?>
-                            </h6>
-                        </div>
-                        <div class="depo-content">
-                            <p style="">
-                                <?php 
-                                    the_excerpt();
-                                ?>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <?php endwhile; else: endif; wp_reset_postdata();?>
-            </div>
-            <div class="gradiente"></div>
-            <div class="swiper-pagination mt-5"></div>
-        </div>
-
-        <div id="testimonial-carousel" class="carousel slide d-block d-lg-none" data-bs-ride="carousel">
+        <div id="testimonial-carousel" class="carousel slide d-block" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <?php 
                     $args = array (
                         'post_type' => 'depoimento',
-                        'orderby' => 'title',
-                        'order' => 'ASC'
+                        'posts_per_page' => 3,
                     );
                     $testimonial_query = new WP_Query($args);
+                    $counter = 0;
 
                     if($testimonial_query->have_posts()) : 
-                        while ($testimonial_query->have_posts()) : $testimonial_query->the_post();                            
+                        while ($testimonial_query->have_posts()) : $testimonial_query->the_post();
+                            if($counter == 0) :
+                                echo '<div class="carousel-item active">';
+                            else :
+                                echo '<div class="carousel-item">';
+                            endif;
+
                             includeFile('components/card-testimonial.php', 
                                 array(
                                     'directory'=>get_the_post_thumbnail_url($post->ID),
@@ -159,7 +111,14 @@
                                     'text'=>get_the_excerpt($post->ID)
                                 )
                             ); 
+
+                            $counter=$counter+1;
+
+                            echo '</div>';
                         endwhile;
+                    
+                    else :
+                        echo '<h1>Nada</h1>';
                     endif;
                 ?>
             </div>
