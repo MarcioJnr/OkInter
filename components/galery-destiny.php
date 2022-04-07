6cf2b2d5e89d0a52609dcc765ab42414ff782439
kingnaldoo <reinaldosn2016@gmail.com>
2022-04-06 21:29:29 -0300

    fix galery

--- components/galery-destiny.php
+++ components/galery-destiny.php
@@ -1,10 +1,10 @@
-<div class="d-flex gap-3" id="galery">
-    <div class="w-50 d-flex flex-column">
-        <img class="shadow h-50" src="<?php echo $img1 ?>" alt="Berlim"/>
-        <img class="shadow h-50" src="<?php echo $img2 ?>" alt="Berlim"/>
+<div class="d-flex gap-3 shadow" id="galery">
+    <div class="w-50 d-flex flex-column justify-content-between gap-3">
+        <img class=" h-50" src="<?php echo $img1 ?>" alt="Berlim"/>
+        <img class="h-50" src="<?php echo $img2 ?>" alt="Berlim"/>
     </div>
 
     <div class="w-50">
-        <img class="shadow" src="<?php echo $img3 ?>" alt="Berlim"/>
+        <img src="<?php echo $img3 ?>" alt="Berlim"/>
     </div>
 </div>
