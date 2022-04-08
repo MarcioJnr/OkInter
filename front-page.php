13617ca194cb9b0d055e57310c28bcc0421dc710
kingnaldoo <reinaldosn2016@gmail.com>
2022-04-08 16:07:03 -0300

    fix order the elements

--- front-page.php
+++ front-page.php
@@ -69,6 +69,38 @@
         </div>
     </section>
 
+        <!--SEÇÃO BUSCA -->
+    <div class="container-fluid p-5" style="background-color: #ffc86e;">
+        <h1 class="display-4 text-dark text-center" style="font-style: normal; font-weight: 400; font-size: 48px; letter-spacing: 0.02em;">Encontre sua próxima viajem</h1>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-5 col-xl-5 justify-content-center">
+                    <select class="programas-destinos form-select form-select-lg text-center mb-2" aria-label="Default select example" style="width:100%; box-shadow: 6px 6px 16px rgba(0, 0, 0, 0.3); border-radius: 8px;">
+                        <option selected>PROGRAMAS</option>
+                        <option value="1">Idiomas</option>
+                        <option value="2">Universidades no exterior</option>
+                        <option value="3">Intercâmbio em família</option>
+                        <option value="4">Business English</option>
+                    </select>
+                </div>
+                <div class="col-12 col-md-5 col-xl-5">
+                    <select class="programas-destinos form-select form-select-lg text-center mb-2" aria-label="Default select example" style="width:100%; box-shadow: 6px 6px 16px rgba(0, 0, 0, 0.3); border-radius: 8px;">
+                        <option selected class="fw-bold">DESTINOS</option>
+                        <option value="1">Canadá</option>
+                        <option value="2">Alemanha</option>
+                        <option value="3">Irlanda</option>
+                        <option value="4">Iraque</option>
+                    </select>
+                </div>
+                <div class="col-12 col-md-2 col-xl-2">
+                    <button id="btn-buscar" type="button" class="fw-bold text-center text-md-center text-xl-center">Buscar</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
     <!--SEÇÃO PACOTES -->
     <section class="container">
         <div>
@@ -186,7 +218,71 @@
         </div>
     </section>
 
-    <!--SEÇÃO DEPOIMENTOS -->
+    <!--SEÇÃO SERVIÇOS -->
+    <div class="container-fluid text-center" style="background-color:#7B39E9;">
+        <div class="container">
+            <div class="row">
+                <h3 class="servicos mt-5">Serviços</h3>
+                <div class="col-12 col-md-12 col-xl-6 text-center img-aviao mt-4 mb-4">
+                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/service-home.png">
+                </div>
+                <div class="col-12 col-md-12 col-xl-6 text-center">
+                    <h4 class="mt-5 titulo-viajem" >Tudo para sua viajem</h3>
+                    <p class="paragrafo1">Passagens aéreas, seguros, traslado e mais</p>
+                    <p class="paragrafo2 text-center mt-5 mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget diam tincidunt tortor vulputate lectus. Aliquam leo tincidunt vitae ut at. Eget pharetra a vitae vulputate. Aenean sagittis quis urna libero et sed feugiat at quis. Euismod sagittis amet, auctor eleifend. Mauris amet vestibulum viverra tincidunt tincidunt enim vitae luctus. 
+                    Nunc dolor turpis in porta elit ultricies mattis. Massa quis malesuada tempor nisl, odio. Porta est faucibus cursus bibendum sodales cursus et arcu, laoreet.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--SEÇÃO DESTINOS -->
+    <div class="container-fluid text-center" style="background-color:#FFB030;">
+        <div class="container">
+            <div class="row">
+                <h3 class="mt-5" style="color: #7B39E9;">Destinos</h3>
+                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
+                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/irlanda-destinos.png">
+                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Irlanda</h4>
+                </div>
+                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
+                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/canada-destinos.png">
+                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Canadá</h4>
+                </div>
+                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
+                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/alemanha-destinos.png">
+                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Alemanha</h4>
+                </div>
+                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
+                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/malta-destinos.png">
+                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Malta</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
+                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/irlanda-destinos.png">
+                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Irlanda</h4>
+                </div>
+                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
+                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/canada-destinos.png">
+                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Irlanda</h4>
+                </div>
+                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
+                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/alemanha-destinos.png">
+                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Irlanda</h4>
+                </div>
+                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
+                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/malta-destinos.png">
+                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Irlanda</h4>
+                </div>
+            </div>
+        </div>
+        <button id="btn-destinos" type="button" class="mt-3 mb-5 fw-bold text-center">Ver mais</button>
+    </div>
+
+     <!--SEÇÃO DEPOIMENTOS -->
     <section class="container mt-5">
         <div>
             <h3 class="text-center text-xl-center cor-empresa mt-4 mb-3">Depoimentos</h3>
@@ -261,100 +357,6 @@
             <div class="swiper-pagination mt-5"></div>
         </div>
     </section>
-
-
-    <div class="container-fluid p-5" style="background-color: #ffc86e;">
-        <h1 class="display-4 text-dark text-center" style="font-style: normal; font-weight: 400; font-size: 48px; letter-spacing: 0.02em;">Encontre sua próxima viajem</h1>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-5 col-xl-5 justify-content-center">
-                    <select class="programas-destinos form-select form-select-lg text-center mb-2" aria-label="Default select example" style="width:100%; box-shadow: 6px 6px 16px rgba(0, 0, 0, 0.3); border-radius: 8px;">
-                        <option selected>PROGRAMAS</option>
-                        <option value="1">Idiomas</option>
-                        <option value="2">Universidades no exterior</option>
-                        <option value="3">Intercâmbio em família</option>
-                        <option value="4">Business English</option>
-                    </select>
-                </div>
-                <div class="col-12 col-md-5 col-xl-5">
-                    <select class="programas-destinos form-select form-select-lg text-center mb-2" aria-label="Default select example" style="width:100%; box-shadow: 6px 6px 16px rgba(0, 0, 0, 0.3); border-radius: 8px;">
-                        <option selected class="fw-bold">DESTINOS</option>
-                        <option value="1">Canadá</option>
-                        <option value="2">Alemanha</option>
-                        <option value="3">Irlanda</option>
-                        <option value="4">Iraque</option>
-                    </select>
-                </div>
-                <div class="col-12 col-md-2 col-xl-2">
-                    <button id="btn-buscar" type="button" class="fw-bold text-center text-md-center text-xl-center">Buscar</button>
-                </div>
-            </div>
-
-        </div>
-    </div>
-    <div class="container-fluid text-center" style="background-color:#7B39E9;">
-        <div class="container">
-            <div class="row">
-                <h3 class="servicos mt-5">Serviços</h3>
-                <div class="col-12 col-md-12 col-xl-6 text-center img-aviao mt-4 mb-4">
-                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/service-home.png">
-                </div>
-                <div class="col-12 col-md-12 col-xl-6 text-center">
-                    <h4 class="mt-5 titulo-viajem" >Tudo para sua viajem</h3>
-                    <p class="paragrafo1">Passagens aéreas, seguros, traslado e mais</p>
-                    <p class="paragrafo2 text-center mt-5 mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget diam tincidunt tortor vulputate lectus. Aliquam leo tincidunt vitae ut at. Eget pharetra a vitae vulputate. Aenean sagittis quis urna libero et sed feugiat at quis. Euismod sagittis amet, auctor eleifend. Mauris amet vestibulum viverra tincidunt tincidunt enim vitae luctus. 
-                    Nunc dolor turpis in porta elit ultricies mattis. Massa quis malesuada tempor nisl, odio. Porta est faucibus cursus bibendum sodales cursus et arcu, laoreet.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="container-fluid text-center" style="background-color:#FFB030;">
-        <div class="container">
-            <div class="row">
-                <h3 class="mt-5" style="color: #7B39E9;">Destinos</h3>
-                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
-                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/irlanda-destinos.png">
-                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Irlanda</h4>
-                </div>
-                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
-                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/canada-destinos.png">
-                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Canadá</h4>
-                </div>
-                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
-                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/alemanha-destinos.png">
-                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Alemanha</h4>
-                </div>
-                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
-                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/malta-destinos.png">
-                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Malta</h4>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
-                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/irlanda-destinos.png">
-                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Irlanda</h4>
-                </div>
-                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
-                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/canada-destinos.png">
-                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Irlanda</h4>
-                </div>
-                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
-                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/alemanha-destinos.png">
-                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Irlanda</h4>
-                </div>
-                <div class="col-12 col-md-6 col-xl-3 mt-4 mb-5">
-                    <img src="<?php echo get_template_directory_uri(  );?>/assets/images/malta-destinos.png">
-                    <h4 class="text-center" style="margin-top:-60px; backdrop-filter: blur(0px); color: white; font-size: 40px;">Irlanda</h4>
-                </div>
-            </div>
-        </div>
-        <button id="btn-destinos" type="button" class="mt-3 mb-5 fw-bold text-center">Ver mais</button>
-    </div>
 
 
     <div class="container">
