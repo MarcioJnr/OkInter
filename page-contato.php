a47ea0f87b38df81e00070cc792fe668b64f8d2a
kingnaldoo <reinaldosn2016@gmail.com>
2022-04-09 16:12:12 -0300

    falta modal de passagens

--- page-contato.php
+++ page-contato.php
@@ -92,21 +92,5 @@ filter: drop-shadow(6px 6px 16px rgba(0, 0, 0, 0.3)); border-radius: 8px;" allow
     
 </main>
 
-<div class="modal fade" id="staticDropdownteste" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog rounded-3 modal-dialog-centered">
-    <div class="modal-content rounded-3 rounded-top">
-      <div class="modal-header">
-          <div>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              <h2 class="cor-empresa mt-3 mb-4" style="line-height: ;">Falar com o consultor</h3>
-          </div>
-      </div>
-      <div class="modal-body">
-      <?php echo do_shortcode('[contact-form-7 id="5" title="Formulário de contato"]');?>
-      </div>
-      
-    </div>
-  </div>
-</div>
-
+//ADICIONAR MODAL DE PASSAGENS
 <?php get_footer(); ?>
