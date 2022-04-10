4ce77fa135a4de5ad798d04bf3b0b2ce9d7be5c6
kingnaldoo <reinaldosn2016@gmail.com>
2022-04-10 00:33:20 -0300

    removed card destiny

--- components/card-destiny.php
+++
@@ -1,4 +0,0 @@
-<a href="<?php echo $href ?>" id="card-destiny" class="border-0 shadow position-relative">
-    <img class="rounded-3 w-100" src="<?php echo $directoryUrl ?>" alt="<?php echo $destinyName ?>"/>
-    <h3 class="card-text-background position-absolute bottom-0 text-center text-white"><?php echo $destinyName ?></h3>
-</a>
