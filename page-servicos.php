30c699116867875a74da7aa91773ab102e7b5939
kingnaldoo <reinaldosn2016@gmail.com>
2022-04-10 12:08:58 -0300

    create modal component

--- page-servicos.php
+++ page-servicos.php
@@ -393,41 +393,10 @@
   </div>
 </main>
 
-
-<div class="modal fade" id="staticDropdownteste" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog rounded-3 modal-dialog-centered">
-    <div class="modal-content rounded-3 rounded-top">
-      <div class="modal-header">
-            <div>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                <h2 class="cor-empresa mt-3 mb-4" style="line-height: ;">Falar com o consultor</h3>
-            </div>
-      </div>
-      <div class="modal-body">
-      <?php echo do_shortcode('[contact-form-7 id="174" title="Formulario de contato"]');?>
-      </div>
-      
-    </div>
-  </div>
-</div>
-
-
-<div class="modal fade" id="staticDropdownCotacao" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog rounded-3 modal-dialog-centered">
-    <div class="modal-content rounded-3 rounded-top">
-      <div class="modal-header">
-            <div>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                <h2 class="cor-empresa mt-3 mb-4" style="line-height: ;">Cotação de passagem áerea</h3>
-            </div>
-      </div>
-      <div class="modal-body">
-      <?php echo do_shortcode('[contact-form-7 id="175" title="form cotacao"]');?>
-      </div>
-    </div>
-  </div>
-</div>
-
+<?php 
+  includeFile('components/modal-contato.php', array());
+  includeFile('components/modal-cotacao.php', array());
+?>
 
 
 <?php get_footer(); ?>
