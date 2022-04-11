9c9933081c61ba2d7195658760d39f09b291c34b
kingnaldoo <reinaldosn2016@gmail.com>
2022-04-11 20:53:30 -0300

    add pagination

--- page-resultados-da-busca.php
+++ page-resultados-da-busca.php
@@ -23,13 +23,14 @@
             $searchQuerySplit = explode('][', $paramsSplit[0]);
             $queryPrograma = $searchQuerySplit[0];
             $queryDestino = $searchQuerySplit[1];
-            $pagedQuery = ($paramsSplit[1]) ? $paramsSplit[1] : 1;
+            $paged = (get_query_var('paged')) ? absint( get_query_var('paged')) : 1;
+            //busca pelos pacotes com 2 parametros
             $args = array (
                 'post_type' => 'pacote',
                 'orderby' => 'title',
                 'order' => 'ASC',
                 'posts_per_page' => 4,
-                'paged'=> $pagedQuery,
+                'paged'=> $paged,
                 'tax_query' => array(
                     'relation' => 'AND',
                     array(
@@ -45,7 +46,8 @@
                 )
             );
             $busca_query = new WP_Query($args);
-
+            // print_r(get_the_terms( $post->ID , 'paispacote' ));
+            
             if($busca_query->have_posts()) : 
                 while ($busca_query->have_posts()) : $busca_query->the_post();
                     includeFile('components/post-search-result.php', 
@@ -53,21 +55,29 @@
                             'title'=>get_the_title($post->ID), 
                             'description'=>get_the_excerpt($post->ID),
                             'srcImg'=>get_the_post_thumbnail_url($post->ID),
-                            'href'=>get_the_permalink($post->ID)
+                            'href'=>get_home_url() . '/destinos/'
                         )
                     ); 
                 endwhile;
             endif;
         ?>
 
-        <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center gap-3">
-                <li class="page-item"><a class="page-link rounded-circle" href="#"><</a></li>
-                <li class="page-item active"><a class="page-link rounded-circle" href="<?php echo get_rest_url($post->ID) ?>">1</a></li>
-                <li class="page-item"><a class="page-link rounded-circle" href="#">2</a></li>
-                <li class="page-item"><a class="page-link rounded-circle" href="#">3</a></li>
-                <li class="page-item"><a class="page-link rounded-circle" href="#">></a></li>
-            </ul>
+        <nav class="pagination d-flex justify-content-center w-100">
+            <div class="pagination-wrapper d-flex justify-content-around">
+                <?php 
+                    $url = get_home_url($post->ID) . '/resultados-da-busca/?results=' . $queryPrograma . '%5D%5B' . $queryDestino;
+                    $big = 999999999;
+                    $pagination_args = array(
+                        'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+                        'format' => '?paged=%#%',
+                        'current' => max(1, get_query_var('paged')),
+                        'total' => $busca_query->max_num_pages,
+                        'prev_text' => '<',
+                        'next_text' => '>'
+                    );
+                    echo paginate_links( $pagination_args );
+                ?>
+            </div>
         </nav>
     </div>
 </div>
